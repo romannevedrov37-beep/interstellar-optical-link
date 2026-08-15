11fe3dac1fcbfea28a906242760dc87bacdbcf1d
romannevedrov37-beep <romannevedrov37@gmail.com>
2026-08-15 17:17:31 +0700

    Use precise Alpha Centauri distance in PyCom comparison (was rounded to 3 sig figs)

--- src/experiments/compare_photon_rate.py
+++ src/experiments/compare_photon_rate.py
@@ -12,7 +12,7 @@ D_r = 1.0        # приёмный телескоп, м
 D_t = 0.3        # передающая апертура, м
 P = 1e9          # мощность лазера, Вт
 wavelength = 1.55e-6  # м
-R = 4.01e16      # м
+R = 4.0113497203742592e16 # м
 
 my_power = received_power(P, D_t, D_r, wavelength, R)
 my_result = photons_per_second(my_power, wavelength)
